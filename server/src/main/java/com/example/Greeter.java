3c14198e41fbdf14f2a7f5eb253daf52befc1a80
vasudevas9030 <161864552+vasudevas9030@users.noreply.github.com>
2024-07-12 11:22:03 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -8,9 +8,7 @@ public class Greeter {
   /**
    * This is a constructor.
    */
-  public Greeter() {
 
-  }
 
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
